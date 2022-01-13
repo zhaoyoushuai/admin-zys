774eaf0c3ff2ef4ebdb324262d5bbb25cf2dbb0c
zhaoyoushuai <zhaoyoushuai06508@51testing.com>
2022-01-13 10:53:11 +0800

    feat: project init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    this is about
     <i-icon icon="home" />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <el-button>查 询</el-button>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home">this is home</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
